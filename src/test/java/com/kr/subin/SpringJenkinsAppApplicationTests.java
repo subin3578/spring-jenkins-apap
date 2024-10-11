adc1165559890cc064f35a011059b7c98e855ec5
unknown <ghkdtnqls95@gmail.com>
2024-10-11 11:26:53 +0900

    jenkins practice

---
+++ src/test/java/com/kr/subin/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.subin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
